f0b4259359b78352d18d5efd45fb17eb14ef1001
rafael.durgante <rafaeldurgante@gmail.com>
2024-11-20 19:19:51 -0300

    Add xfail to test_invoice_details due to BUG-1: Incorrect currency format

--- tests/test_invoice_details.py
+++ tests/test_invoice_details.py
@@ -1,7 +1,12 @@
+import pytest
 from pages.invoice_page import InvoicePage
 from pages.login_page import LoginPage
 from utils.test_data import VALID_CREDENTIALS, INVOICE_DETAILS
 
+@pytest.mark.xfail(reason="BUG-1: Incorrect Currency Format in 'Tax&VAT' and 'TotalAmount' Fields")
+# Ref: https://trello.com/c/9Wl70i4V/7-bug-1-incorrect-currency-format-in-taxvat-and-totalamount-fields
+# This test currently fails because the values in the 'Tax&VAT' and 'TotalAmount' fields
+# are displayed without decimal places, such as '$19' instead of '$19.00'.
 def test_invoice_details(browser):
     page = browser.new_page()
     login_page = LoginPage(page)
